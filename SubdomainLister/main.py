e507742a8ec432133dc77d99d6c0487d0b97e7d0
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-04-21 17:51:21 +0100

    Implement simple subdomain enumeration script with connection error handling

--- SubdomainLister/main.py
+++ SubdomainLister/main.py
@@ -1,1 +1,21 @@
 import requests
+
+def make_request(url):
+    try:
+        return requests.get(url)
+    except requests.exceptions.ConnectionError:
+        pass
+    
+    
+target_input= "google.com"
+
+with open("SubdomainLister\subdomainlist.txt","r") as file:
+    for word in file:
+        word=word.strip() #for cleaning
+        url="http://"+ word + "." + target_input
+        response = make_request(url)
+        if response:
+            print(f"Found subdomain ---> {url}")
+            
+        else:
+            print(response)
